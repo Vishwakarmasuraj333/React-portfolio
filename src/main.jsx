55f7c43b3fc948b56b688b919005d62a7e5cb359
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2025-10-18 09:39:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // make sure the path is correct
+import "./index.css";         // make sure the path is correct
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
