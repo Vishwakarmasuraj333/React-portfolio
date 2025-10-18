c2e44352fdf6a5efc8eb5663d66be8b861eae271
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2025-10-18 09:49:30 +0530

    Ensure base path is set for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/React-portfolio/', // Set base path to match your repository name
+  base: '/React-portfolio/', // <- important for GitHub Pages
   plugins: [react()],
 });
-
